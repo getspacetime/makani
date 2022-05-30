00d10e4dd64cf325c5a478f28100eda5701359c6
Cody Mullins <joshua.c.mullins@gmail.com>
2022-05-29 20:12:58 -0400

    integrate tailwind cli

---
+++ src/Makani/MakaniColor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makani
+{
+    public enum MkColor
+    {
+        Default,
+        Primary,
+        Secondary
+    }
+
+    public enum MkSize
+    {
+        Default,
+        Small,
+        Medium,
+        Large
+    }
+}
