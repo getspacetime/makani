317899d4d4832501ca0240801e0c66d7035f3ad0
Cody Mullins <joshua.c.mullins@gmail.com>
2022-06-11 14:06:29 -0400

    Update tailwind.config.js

--- src/Makani/wwwroot/tailwind.config.js
+++ src/Makani/wwwroot/tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ["../*.razor",  "../**/*.razor", "../**/**/*.razor"],
   theme: {
-    extend: {},
+      extend: {},
+      colors: {
+          // Now we build the full color palette, using all colors available
+          // as shown at this link: https://tailwindcss.com/docs/customizing-colors#color-palette-reference
+          transparent: "transparent",
+          current: "currentColor",
+          black: "#000",
+          white: "#fff",
+          gray: colors.gray,
+          primary: colors.indigo,
+          secondary: colors.gray,
+          success: colors.emerald,
+          error: colors.rose,
+          info: colors.sky,
+          warning: colors.amber
+      },
   },
   plugins: [],
 }
