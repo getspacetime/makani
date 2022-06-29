f990a022fb7b0c8978a6f6e96d70cee4b254d891
Cody Mullins <joshua.c.mullins@gmail.com>
2022-06-29 11:05:53 -0400

    add regex validator

---
+++ src/Makani.Tests/Validators/RegexValidatorTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Makani.Components.Forms.Validators;
+
+namespace Makani.Tests.Validators
+{
+    public class RegexValidatorTests
+    {
+        private const string OnlyLetters = @"^[a-zA-Z]+$";
+
+        [Fact]
+        public void Validate_ShouldBeTrue_WhenInputIsNull()
+        {
+            var sut = new RegexValidator(new Regex("\\G"));
+            var actual = sut.ExecuteValidate(null);
+            actual.Valid.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("test")]
+        [InlineData("t")]
+        public void Validate_ShouldBeTrue_WhenPatternMatches(string input)
+        {
+            var sut = new RegexValidator(new Regex(OnlyLetters));
+            var actual = sut.ExecuteValidate(input);
+            actual.Valid.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("test123")]
+        [InlineData("test ")]
+        [InlineData(" test")]
+        [InlineData(" ")]
+        public void Validate_ShouldBeFalse_WhenPatternDoesNotMatch(string input)
+        {
+            var sut = new RegexValidator(new Regex(OnlyLetters));
+            var actual = sut.ExecuteValidate(input);
+            actual.Valid.Should().BeFalse();
+        }
+    }
+}
---
+++ src/Makani/Components/Forms/Validators/RegexValidator.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Makani.Components.Forms.Validators;
+
+public class RegexValidator : EntryValidator
+{
+    private readonly Regex _rx;
+
+    public RegexValidator(Regex rx)
+    {
+        _rx = rx;
+    }
+
+    public override ValidationResult ExecuteValidate(string? input)
+    {
+        if (input == null)
+        {
+            return new ValidationResult(true);
+        }
+
+        var match = _rx.Match(input);
+
+        return new ValidationResult(match.Success, match.Success == false ? $"{input} does not match the regular expression" : null);
+    }
+}
